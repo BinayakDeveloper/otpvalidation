fb1306a6e17e12c5862a934754a484c31980fb12
BinayakDeveloper <banjarajajabar@gmail.com>
2024-01-30 23:39:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://WebDeveloper:webdev@maincluster.cq4nipw.mongodb.net/?retryWrites=true&w=majority",
+    {
+      dbName: "otp",
+    }
+  )
+  .then(() => {
+    console.log("DB Connected");
+  });
+
+const schema = mongoose.Schema({
+  otp: Number,
+});
+
+const model = mongoose.model("model", schema, "otp");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  let otp = await model.create({ otp: 123 });
+  let otpId = otp.id;
+  console.log(otpId);
+  setTimeout(() => {
+    async function deleteOtp() {
+      await model.findByIdAndDelete(otpId);
+    }
+    deleteOtp();
+  }, 15000);
+
+  res.end();
+});
+
+app.listen(500);
